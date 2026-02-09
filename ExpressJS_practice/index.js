fb9284a081478e6c4943621acaa76860f637c7ad
srijanisaDev <srijankumar770@gmail.com>
2026-02-09 17:53:22 +0530

    Middleware

--- ExpressJS_practice/index.js
+++ ExpressJS_practice/index.js
@@ -3,7 +3,20 @@ const app = express();
 const port = 3001;
 
 app.get("/",(req,res) => {
-    res.send("Hello World !!");
+    res.send("<h1>Hello Srijan!!</h1>");
+})
+
+app.get("/about", (req, res) => {
+    res.send(`
+        <h3>About Me</h3>
+        <h5>
+            <p>Hello I am Srijan and I am currently learning HTTP requests!!</p>
+        </h5>
+    `);
+});
+
+app.get("/contact",(req,res) => {
+    res.send("<h1>+17232214</h1>");
 })
 
 app.listen(port , () => {
